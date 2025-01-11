41eb99f55cfa6d1340ca06820964eccb6ac56158
engmwamba <118115109+engmwamba@users.noreply.github.com>
2025-01-11 22:55:15 +0300

    Background Update

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-// Smooth Scrolling
-document.querySelectorAll("a[href^='#']").forEach((anchor) => {
-    anchor.addEventListener("click", (e) => {
-      e.preventDefault();
-      document.querySelector(anchor.getAttribute("href")).scrollIntoView({ behavior: "smooth" });
+document.addEventListener('DOMContentLoaded', () => {
+    const tabs = document.querySelectorAll('.tabs li');
+    const contents = document.querySelectorAll('.tab-content');
+  
+    tabs.forEach(tab => {
+      tab.addEventListener('click', () => {
+        tabs.forEach(t => t.classList.remove('active'));
+        contents.forEach(content => content.classList.remove('active'));
+  
+        tab.classList.add('active');
+        document.getElementById(tab.dataset.tab).classList.add('active');
+      });
     });
   });
   
