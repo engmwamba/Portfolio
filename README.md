17fa1ee9cb4cafa6c29b879a1ced6e784a3707a8
engmwamba <118115109+engmwamba@users.noreply.github.com>
2025-01-11 22:24:45 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# portfolio
+ This portfolio is a summary of experience, skills and projects I have gained throughout the years 
---
+++ script.js
@@ -0,0 +1,8 @@
+// Smooth Scrolling
+document.querySelectorAll("a[href^='#']").forEach((anchor) => {
+    anchor.addEventListener("click", (e) => {
+      e.preventDefault();
+      document.querySelector(anchor.getAttribute("href")).scrollIntoView({ behavior: "smooth" });
+    });
+  });
+  
